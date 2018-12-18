4e9bb390472be8a7988a5549a0b2a31964103764
unknown <dawiddeveloper@gmail.com>
2018-12-18 13:34:00 +0100

    responsivness bugs removed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,28 @@
 import '../scss/style.scss';
 import '../scss/fontello.css';
-import {TweenLite, TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
+import {
+    TweenLite,
+    TweenMax,
+    Power2,
+    TimelineLite
+} from "gsap/TweenMax";
 
 
 // slider
 let slider = document.querySelector(".slider");
 let slides = Array.from(document.querySelectorAll('.slider__item'));
-let h1 = document.querySelector('.slider__h1');
-let description = document.querySelector('.slider__description');
+let h1 = document.querySelectorAll('.slider__h1');
+let description = document.querySelectorAll('.slider__description');
 let button = document.querySelector('.btn');
 // controls
 let dots = Array.from(document.querySelectorAll('.icon-circle-empty'));
-let tl = new TimelineMax({repeat: 123});
-let tl_text = new TimelineMax({repeat: 123});
+let tl = new TimelineMax({
+    repeat: 123
+});
+let tl_text = new TimelineMax({
+    repeat: 123
+});
+
 
 
 
@@ -27,31 +37,114 @@ window.addEventListener('load', setSliderHeight)
 
 
 
-TweenLite.set(slides[1], {opacity: 0});
+TweenLite.set(slides[1], {
+    opacity: 0
+});
 
 
 tl
-.set(dots[1], {textShadow: 'none'})
-.set(dots[0], {textShadow: '0 0 10px black'})
-.from(slides[0], 1, {opacity: 0})
-
-
-
-
-
-
-.fromTo(h1,1,{transform: "rotateX(-150deg)", transformOriginin:'20% 40%'}, {transform: "rotateX(0)", opacity: 1, transformOriginin: '20% 40%', ease: Bounce.easeOut})
-
-.fromTo(description,3,{transform: "translateX(10vw)", opacity: 0}, {transform: "translateX(0)", opacity: 1, ease:  Elastic.easeOut.config(1, 0.3)},'-=1')
-.fromTo(button,3,{transform: "translateX(2vw)", opacity: 0}, {transform: "translateX(0)", opacity: 1, ease:  Elastic.easeOut.config(1, 0.3)},'-=2')
-
-
-.to(slides[0],1,{opacity: 1}, '+=3')
-.to(slides[0],1,{opacity: 0})
-.set(dots[0], {textShadow: 'none'})
-.set(dots[1], {textShadow: '0 0 10px black'})
-.to(slides[1],1,{opacity: 1})
-.to(slides[1],1,{opacity: 0}, '+=3')
-
-
-
+    .set(h1[1], {
+        display: 'none'
+    })
+    .set(description[1], {
+        display: 'none'
+    })
+    .set(dots[1], {
+        textShadow: 'none'
+    })
+    .set(dots[0], {
+        textShadow: '0 0 10px black'
+    })
+    .from(slides[0], 1, {
+        opacity: 0
+    })
+    .fromTo(h1[0], 1, {
+        transform: "rotateX(-150deg)",
+        transformOriginin: '20% 40%'
+    }, {
+        transform: "rotateX(0)",
+        opacity: 1,
+        transformOriginin: '20% 40%',
+        ease: Bounce.easeOut
+    })
+    .fromTo(description[0], 3, {
+        transform: "translateX(10vw)",
+        opacity: 0
+    }, {
+        transform: "translateX(0)",
+        opacity: 1,
+        ease: Elastic.easeOut.config(1, 0.3)
+    }, '-=1')
+    .fromTo(button, 3, {
+        transform: "translateX(2vw)",
+        opacity: 0
+    }, {
+        transform: "translateX(0)",
+        opacity: 1,
+        ease: Elastic.easeOut.config(1, 0.3)
+    }, '-=2')
+    .set(h1[0], {
+        display: 'none'
+    })
+    .set(description[0], {
+        display: 'none'
+    })
+    .set(button, {
+        visibility: 'hidden'
+    })
+    .set(h1[1], {
+        display: 'block'
+    })
+    .set(description[1], {
+        display: 'block'
+    })
+    .to(slides[0], 1, {
+        opacity: 1
+    })
+    .to(slides[0], 1, {
+        opacity: 0
+    })
+    .set(dots[0], {
+        textShadow: 'none'
+    })
+    .set(dots[1], {
+        textShadow: '0 0 10px black'
+    })
+    .to(slides[1], 1, {
+        opacity: 1
+    })
+    .fromTo(h1[1], 1, {
+        transform: "rotateX(-150deg)",
+        transformOriginin: '20% 40%'
+    }, {
+        transform: "rotateX(0)",
+        opacity: 1,
+        transformOriginin: '20% 40%',
+        ease: Bounce.easeOut
+    })
+    .fromTo(description[1], 3, {
+        transform: "translateX(10vw)",
+        opacity: 0
+    }, {
+        transform: "translateX(0)",
+        opacity: 1,
+        ease: Elastic.easeOut.config(1, 0.3)
+    }, '-=1')
+    .fromTo(button, 3, {
+        visibility: 'visible',
+        transform: "translateX(2vw)",
+        opacity: 0
+    }, {
+        transform: "translateX(0)",
+        opacity: 1,
+        ease: Elastic.easeOut.config(1, 0.3)
+    }, '-=2')
+    .to(slides[1], 1, {
+        opacity: 0
+    })
+    .set(h1[0], {
+        display: 'block'
+    })
+    .set(description[0], {
+        display: 'block'
+    })
--- dist/scripts.66fe92b2.js
+++ dist/scripts.66fe92b2.js
@@ -10736,8 +10736,8 @@ var _TweenMax = require("gsap/TweenMax");
 // slider
 var slider = document.querySelector(".slider");
 var slides = Array.from(document.querySelectorAll('.slider__item'));
-var h1 = document.querySelector('.slider__h1');
-var description = document.querySelector('.slider__description');
+var h1 = document.querySelectorAll('.slider__h1');
+var description = document.querySelectorAll('.slider__description');
 var button = document.querySelector('.btn'); // controls
 
 var dots = Array.from(document.querySelectorAll('.icon-circle-empty'));
@@ -10760,13 +10760,17 @@ _TweenMax.TweenLite.set(slides[1], {
   opacity: 0
 });
 
-tl.set(dots[1], {
+tl.set(h1[1], {
+  display: 'none'
+}).set(description[1], {
+  display: 'none'
+}).set(dots[1], {
   textShadow: 'none'
 }).set(dots[0], {
   textShadow: '0 0 10px black'
 }).from(slides[0], 1, {
   opacity: 0
-}).fromTo(h1, 1, {
+}).fromTo(h1[0], 1, {
   transform: "rotateX(-150deg)",
   transformOriginin: '20% 40%'
 }, {
@@ -10774,7 +10778,7 @@ tl.set(dots[1], {
   opacity: 1,
   transformOriginin: '20% 40%',
   ease: Bounce.easeOut
-}).fromTo(description, 3, {
+}).fromTo(description[0], 3, {
   transform: "translateX(10vw)",
   opacity: 0
 }, {
@@ -10788,9 +10792,19 @@ tl.set(dots[1], {
   transform: "translateX(0)",
   opacity: 1,
   ease: Elastic.easeOut.config(1, 0.3)
-}, '-=2').to(slides[0], 1, {
+}, '-=2').set(h1[0], {
+  display: 'none'
+}).set(description[0], {
+  display: 'none'
+}).set(button, {
+  visibility: 'hidden'
+}).set(h1[1], {
+  display: 'block'
+}).set(description[1], {
+  display: 'block'
+}).to(slides[0], 1, {
   opacity: 1
-}, '+=3').to(slides[0], 1, {
+}).to(slides[0], 1, {
   opacity: 0
 }).set(dots[0], {
   textShadow: 'none'
@@ -10798,9 +10812,36 @@ tl.set(dots[1], {
   textShadow: '0 0 10px black'
 }).to(slides[1], 1, {
   opacity: 1
-}).to(slides[1], 1, {
+}).fromTo(h1[1], 1, {
+  transform: "rotateX(-150deg)",
+  transformOriginin: '20% 40%'
+}, {
+  transform: "rotateX(0)",
+  opacity: 1,
+  transformOriginin: '20% 40%',
+  ease: Bounce.easeOut
+}).fromTo(description[1], 3, {
+  transform: "translateX(10vw)",
   opacity: 0
-}, '+=3');
+}, {
+  transform: "translateX(0)",
+  opacity: 1,
+  ease: Elastic.easeOut.config(1, 0.3)
+}, '-=1').fromTo(button, 3, {
+  visibility: 'visible',
+  transform: "translateX(2vw)",
+  opacity: 0
+}, {
+  transform: "translateX(0)",
+  opacity: 1,
+  ease: Elastic.easeOut.config(1, 0.3)
+}, '-=2').to(slides[1], 1, {
+  opacity: 0
+}).set(h1[0], {
+  display: 'block'
+}).set(description[0], {
+  display: 'block'
+});
 },{"../scss/style.scss":"assets/scss/style.scss","../scss/fontello.css":"assets/scss/fontello.css","gsap/TweenMax":"node_modules/gsap/TweenMax.js"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -10828,7 +10869,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53967" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49440" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
